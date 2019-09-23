1054c88837f9415a1a169983e5eb5dc954dd4630
Hana Lee <hanalee@Hanas-MacBook-Pro.local>
2019-09-23 15:30:23 -0700

    fixed syntax and typo error

---
+++ lib/__init__.py
@@ -0,0 +1,5 @@
+from lib.options import Options
+from lib.playlist_controller import PlaylistController
+from lib.user_controller import UserController
+from lib.song_controller import SongController
+from lib.file_controller import FileController
